701fac46a12fd0d816061e225c56500c65cb31aa
sakhahidayah <sakhahidayahramadhan@gmail.com>
2026-04-05 19:11:05 +0700

    first commit

---
+++ manhwa-frontend/src/layout/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header className="bg-gray-800">
+        <div className="container mx-auto lg:px-8 px-4 py-4">
+          <h1 className="text-white text-2xl xl:text-xl font-bold">My Manhwa App</h1>
+        </div>
+      </header>
+    </>
+  );
+}
