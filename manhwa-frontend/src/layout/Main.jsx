3ada6642a2a4cdf36f8e76e566b8bfa0a6af47df
sakhahidayah <sakhahidayahramadhan@gmail.com>
2026-04-21 21:40:31 +0700

    delete lokal api

--- manhwa-frontend/src/layout/Main.jsx
+++ manhwa-frontend/src/layout/Main.jsx
@@ -1,6 +1,4 @@
-import FilterGenre from "../features/FilterGenre";
 import Cards from "../components/Cards";
-
 export default function Main() {
   return (
     <main className="container mx-auto lg:px-8 px-2 py-2 bg-[#09090b] min-h-screen overflow-hidden text-white">
